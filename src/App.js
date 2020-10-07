928178e40c4897d8db9d3dc93cdb1bb71ac55fa0
Beizeng <50338897+BeizengVue92@users.noreply.github.com>
2020-10-06 22:29:58 -0500

    committing...

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+
+const api = {
+  base:
+    "https://api.weatherapi.com/v1/current.json?key=6f3435eca3d64b24b40135728200610&q=",
+};
 
 function App() {
+  const [query, setQuery] = useState("");
+  const [weather, setWeather] = useState("");
+
+  const search = (evt) => {
+    if (evt.key === "Enter") {
+      fetch(`${api.base}${query}`)
+        .then((res) => res.json())
+        .then((result) => {
+          setWeather(result);
+          setQuery("");
+          console.log(result);
+        });
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="appName">Weather App</div>
+      <div className="search-box">
+        <input
+          type="text"
+          className="search-bar"
+          placeholder="Search..."
+          onChange={(e) => setQuery(e.target.value)}
+          value={query}
+          onKeyPress={search}
+        />
+      </div>
+      {typeof weather.location != "undefined" &&
+      typeof weather.current != "undefined" ? (
+        <div>
+          <div className="location-box">
+            <div className="location">
+              {weather["location"].name}, {weather["location"].region}
+            </div>
+            <div className="local-time">{weather["location"].localtime}</div>
+          </div>
+          <div className="current-box">
+            <div className="current">
+              {Math.round(weather["current"].temp_f)}°F
+              <div className="skyDetails">
+                {weather["current"]["condition"].text}
+              </div>
+            </div>
+          </div>
+        </div>
+      ) : (
+        ""
+      )}
     </div>
   );
 }
